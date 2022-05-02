4eb030065991cb9473e3ac0834026ec60b5df352
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2022-05-02 13:02:07 +0200

    Add layout components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navigation/navbar/navbar.component';
+import { NavbarMobileComponent } from './layout/navigation/navbar-mobile/navbar-mobile.component';
+import { HomeComponent } from './views/home/home.component';
+import { CountriesComponent } from './views/countries/countries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent,
+    NavbarMobileComponent,
+    HomeComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
